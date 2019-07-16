4c20ac86ce3ad085b7ddea97768cac575c21845d
Martine Enger <marenger@student.matnat.uio.no>
2019-07-16 10:52:25 +0200

    Lagt på autentisering med intern issuer

--- src/main/java/no/nav/familie/ks/oppslag/Launcher.java
+++ src/main/java/no/nav/familie/ks/oppslag/Launcher.java
@@ -1,9 +1,11 @@
 package no.nav.familie.ks.oppslag;
 
+import no.nav.security.spring.oidc.api.EnableOIDCTokenValidation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableOIDCTokenValidation(ignore="org.springframework")
 public class Launcher {
 
     public static void main(String[] args) {
--- src/main/java/no/nav/familie/ks/oppslag/personopplysning/PersonopplysningerController.java
+++ src/main/java/no/nav/familie/ks/oppslag/personopplysning/PersonopplysningerController.java
@@ -3,6 +3,7 @@ package no.nav.familie.ks.oppslag.personopplysning;
 import no.nav.familie.ks.oppslag.felles.MDCOperations;
 import no.nav.familie.ks.oppslag.personopplysning.domene.AktørId;
 import no.nav.familie.ks.oppslag.personopplysning.domene.PersonhistorikkInfo;
+import no.nav.security.oidc.api.ProtectedWithClaims;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 @RestController
+@ProtectedWithClaims(issuer = "intern")
 @RequestMapping("/api/personopplysning")
 public class PersonopplysningerController {
 
