00bfbeafbba5a4493c94462c14ce3a7a664e0f00
Martine Enger <marenger@student.matnat.uio.no>
2019-07-16 14:47:44 +0200

    Flytt dummy-credentials over i dev-profil. Hentes ellers fra vault. Følg navnekonvensjon

--- src/main/java/no/nav/familie/ks/oppslag/felles/ws/sts/SecurityConstants.java
+++ src/main/java/no/nav/familie/ks/oppslag/felles/ws/sts/SecurityConstants.java
@@ -3,8 +3,8 @@ package no.nav.familie.ks.oppslag.felles.ws.sts;
 public class SecurityConstants {
 
     public static final String STS_URL_KEY = "SECURITYTOKENSERVICE_URL";
-    public static final String SYSTEMUSER_USERNAME = "credential.username";
-    public static final String SYSTEMUSER_PASSWORD = "credential.password";
+    public static final String SYSTEMUSER_USERNAME = "CREDENTIAL_USERNAME";
+    public static final String SYSTEMUSER_PASSWORD = "CREDENTIAL_PASSWORD";
 
     private SecurityConstants() {
         throw new IllegalAccessError("Skal ikke instansieres");
