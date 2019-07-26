d2f678b7eaf33b7e9aa9ba74e2bbe8f6bbb87a56
Martine Enger <marenger@student.matnat.uio.no>
2019-07-26 09:58:38 +0200

    Mer logg etter 401 mot aktørreg

--- src/main/java/no/nav/familie/ks/oppslag/aktør/internal/AktørregisterClient.java
+++ src/main/java/no/nav/familie/ks/oppslag/aktør/internal/AktørregisterClient.java
@@ -3,6 +3,8 @@ package no.nav.familie.ks.oppslag.aktør.internal;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import no.nav.familie.ks.oppslag.felles.MDCOperations;
 import no.nav.familie.ks.oppslag.felles.rest.StsRestClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 
@@ -22,6 +24,7 @@ public class AktørregisterClient {
     private static final String NAV_CALL_ID = "Nav-Call-Id";
     private static final String NAV_PERSONIDENTER = "Nav-Personidenter";
     private static final String AKTOERID_IDENTGRUPPE = "AktoerId";
+    private static final Logger LOG = LoggerFactory.getLogger(AktørregisterClient.class);
 
     private HttpClient httpClient;
     private StsRestClient stsRestClient;
@@ -42,6 +45,7 @@ public class AktørregisterClient {
     public AktørResponse hentAktørId(String personIdent) {
         URI uri = URI.create(String.format("%s/identer?gjeldende=true&identgruppe=%s", aktørRegisterUrl, AKTOERID_IDENTGRUPPE));
         String systembrukerToken = stsRestClient.getSystemOIDCToken();
+        LOG.info("Token fra STS: {}", systembrukerToken);
 
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(uri)
