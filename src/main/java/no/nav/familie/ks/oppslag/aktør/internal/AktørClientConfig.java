0cbcf01fd81c96ba0bb485504e0fc7d93443532c
Martine Enger <marenger@student.matnat.uio.no>
2019-07-26 10:27:29 +0200

    [kaizen] Tror ikke man trenger enableconfigurationproperties på aktørclientconfig

--- src/main/java/no/nav/familie/ks/oppslag/aktør/internal/AktørClientConfig.java
+++ src/main/java/no/nav/familie/ks/oppslag/aktør/internal/AktørClientConfig.java
@@ -4,12 +4,10 @@ package no.nav.familie.ks.oppslag.aktør.internal;
 import no.nav.familie.ks.oppslag.felles.rest.StsRestClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableConfigurationProperties
 public class AktørClientConfig {
 
     @Bean
