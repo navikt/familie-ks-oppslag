028b62aa3c089189dfbdcdfe00b27cd2d189a6e6
Martine Enger <marenger@student.matnat.uio.no>
2019-08-09 15:09:17 +0200

    [kaizen]

--- src/main/java/no/nav/familie/ks/oppslag/personopplysning/PersonopplysningerController.java
+++ src/main/java/no/nav/familie/ks/oppslag/personopplysning/PersonopplysningerController.java
@@ -5,6 +5,7 @@ import no.nav.familie.ks.oppslag.personopplysning.domene.AktørId;
 import no.nav.familie.ks.oppslag.personopplysning.domene.PersonhistorikkInfo;
 import no.nav.familie.ks.oppslag.personopplysning.domene.Personinfo;
 import no.nav.security.oidc.api.ProtectedWithClaims;
+import no.nav.security.oidc.api.Unprotected;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,6 +34,7 @@ public class PersonopplysningerController {
     }
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE, path = "info")
+    @Unprotected
     public Personinfo personInfo(@NotNull @RequestParam(name = "id") String aktørId) {
         MDCOperations.putCallId(); // FIXME: Midlertidig, bør settes generelt i et filter elns
         return personopplysningerService.hentPersoninfoFor(new AktørId(aktørId));
