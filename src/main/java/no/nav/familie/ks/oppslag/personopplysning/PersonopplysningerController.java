26ca4f17f455e17c3b6092b6d6aa11b0eaa8c2ed
Martine Enger <marenger@student.matnat.uio.no>
2019-08-16 08:32:10 +0200

    [kaizen] Fjerner Unprotected på personinfo-endepkt, test ferdig

--- src/main/java/no/nav/familie/ks/oppslag/personopplysning/PersonopplysningerController.java
+++ src/main/java/no/nav/familie/ks/oppslag/personopplysning/PersonopplysningerController.java
@@ -6,7 +6,6 @@ import no.nav.familie.ks.oppslag.personopplysning.domene.PersonhistorikkInfo;
 import no.nav.familie.ks.oppslag.personopplysning.domene.Personinfo;
 import no.nav.security.oidc.api.ProtectedWithClaims;
 import org.springframework.format.annotation.DateTimeFormat;
-import no.nav.security.oidc.api.Unprotected;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -38,7 +37,6 @@ public class PersonopplysningerController {
     }
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE, path = "info")
-    @Unprotected
     public Personinfo personInfo(@NotNull @RequestParam(name = "id") String aktørId) {
         MDCOperations.putCallId(); // FIXME: Midlertidig, bør settes generelt i et filter elns
         return personopplysningerService.hentPersoninfoFor(new AktørId(aktørId));
