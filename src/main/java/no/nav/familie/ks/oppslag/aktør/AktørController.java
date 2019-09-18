679368ec523bef8dbb2017a60e620d494e76c6c4
Henning Håkonsen <henninghaakonsen@me.com>
2019-09-18 10:17:30 +0200

    Legger til endepunkt for å veksle inn aktørid til fnr.

--- src/main/java/no/nav/familie/ks/oppslag/aktør/AktørController.java
+++ src/main/java/no/nav/familie/ks/oppslag/aktør/AktørController.java
@@ -1,5 +1,6 @@
 package no.nav.familie.ks.oppslag.aktør;
 
+import no.nav.familie.ks.oppslag.personopplysning.domene.AktørId;
 import no.nav.security.oidc.api.ProtectedWithClaims;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -22,5 +23,10 @@ public class AktørController {
     @GetMapping
     public String getAktørIdForPersonIdent(@NotNull @RequestHeader(name = "Nav-Personident") String personIdent) {
         return aktørService.getAktørId(personIdent);
+    }
+
+    @GetMapping(path = "/fraaktorid")
+    public String getPersonIdentForAktørId(@NotNull @RequestHeader(name = "Nav-Aktorid") String aktørId) {
+        return aktørService.getPersonIdent(new AktørId(aktørId));
     }
 }
