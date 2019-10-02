aa60a5f93c408622052d0350c0c45696a32c1fc2
Henning Håkonsen <henninghaakonsen@me.com>
2019-10-02 15:25:33 +0200

    kaizen

--- src/test/java/no/nav/familie/ks/oppslag/felles/config/StsTestConfig.java
+++ src/test/java/no/nav/familie/ks/oppslag/felles/config/StsTestConfig.java
@@ -1,6 +1,6 @@
 package no.nav.familie.ks.oppslag.felles.config;
 
-import no.nav.familie.http.sts.StsRestClient;
+import no.nav.familie.ks.oppslag.sts.StsRestClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
