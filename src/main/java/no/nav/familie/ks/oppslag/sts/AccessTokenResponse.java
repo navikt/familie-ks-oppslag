6ad7c9011c540b75da074bee15165daaea861db9
Henning Håkonsen <henninghaakonsen@me.com>
2019-10-02 16:25:43 +0200

    kaizen

--- src/main/java/no/nav/familie/ks/oppslag/sts/AccessTokenResponse.java
+++ src/main/java/no/nav/familie/ks/oppslag/sts/AccessTokenResponse.java
@@ -14,6 +14,18 @@ class AccessTokenResponse {
         this.expires_in = expires_in;
     }
 
+    String getAccess_token() {
+        return access_token;
+    }
+
+    public String getToken_type() {
+        return token_type;
+    }
+
+    Long getExpires_in() {
+        return expires_in;
+    }
+
     @Override
     public String toString() {
         return String.format("accessToken: %s, tokenType: %s, expiresInn: %s", access_token, token_type, expires_in);
