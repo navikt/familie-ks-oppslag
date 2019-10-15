87bd2a89d67b798e07c80040204b70b525b76c50
Stig Helge Strøm <stig.helge.strom@nav.no>
2019-10-15 10:40:54 +0200

    Fikset ferdigstill json

--- src/main/java/no/nav/familie/ks/oppslag/dokarkiv/client/DokarkivClient.java
+++ src/main/java/no/nav/familie/ks/oppslag/dokarkiv/client/DokarkivClient.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.Metrics;
 import io.micrometer.core.instrument.Timer;
@@ -33,6 +32,7 @@ public class DokarkivClient {
     private static final String NAV_CONSUMER_ID = "Nav-Consumer-Id";
     private static final String NAV_CALL_ID = "Nav-Call-Id";
     private static final String NAV_PERSONIDENTER = "Nav-Personidenter";
+    public static final String FERDIGSTILL_JOURNALPOST_JSON = "{\"journalfoerendeEnhet\":9999}";
     private final Timer opprettJournalpostResponstid = Metrics.timer("dokarkiv.opprett.respons.tid");
     private final Counter opprettJournalpostSuccess = Metrics.counter("dokarkiv.opprett.response", "status", "success");
     private final Counter opprettJournalpostFailure = Metrics.counter("dokarkiv.opprett.response", "status", "failure");
@@ -85,7 +85,7 @@ public class DokarkivClient {
             HttpResponse<String> httpResponse = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
             opprettJournalpostResponstid.record(System.nanoTime() - startTime, TimeUnit.NANOSECONDS);
 
-            if (httpResponse.statusCode() == HttpStatus.OK.value() || httpResponse.statusCode() == HttpStatus.CREATED.value() ) {
+            if (httpResponse.statusCode() == HttpStatus.OK.value() || httpResponse.statusCode() == HttpStatus.CREATED.value()) {
                 opprettJournalpostSuccess.increment();
                 return objectMapper.readValue(httpResponse.body(), OpprettJournalpostResponse.class);
 
@@ -104,8 +104,6 @@ public class DokarkivClient {
         URI uri = URI.create(String.format("%s/rest/journalpostapi/v1/journalpost/%s/ferdigstill", dokarkivUrl, journalpostId));
         String systembrukerToken = stsRestClient.getSystemOIDCToken();
         try {
-
-
             HttpRequest request = HttpRequest.newBuilder()
                     .uri(uri)
                     .header(ACCEPT, "application/json")
@@ -113,7 +111,7 @@ public class DokarkivClient {
                     .header(NAV_CONSUMER_ID, consumer)
                     .header(NAV_CALL_ID, MDCOperations.getCallId())
                     .header(AUTHORIZATION, "Bearer " + systembrukerToken)
-                    .method("PATCH", HttpRequest.BodyPublishers.ofString("{ \" journalfoerndeEnhet\" :  \"9999\"}"))
+                    .method("PATCH", HttpRequest.BodyPublishers.ofString(FERDIGSTILL_JOURNALPOST_JSON))
                     .timeout(Duration.ofSeconds(20)) // kall tar opptil 8s i preprod.
                     .build();
 
@@ -121,7 +119,7 @@ public class DokarkivClient {
             HttpResponse<String> httpResponse = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
             ferdigstillJournalpostResponstid.record(System.nanoTime() - startTime, TimeUnit.NANOSECONDS);
 
-            if (httpResponse.statusCode() == HttpStatus.OK.value() || httpResponse.statusCode() == HttpStatus.CREATED.value() ) {
+            if (httpResponse.statusCode() == HttpStatus.OK.value() || httpResponse.statusCode() == HttpStatus.CREATED.value()) {
                 ferdigstillJournalpostSuccess.increment();
             } else if (httpResponse.statusCode() == HttpStatus.BAD_REQUEST.value()) {
                 ferdigstillJournalpostFailure.increment();
