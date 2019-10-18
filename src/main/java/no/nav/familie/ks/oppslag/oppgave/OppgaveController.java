d698f32ecf9c52b6bf76b1ca0f047ad8424d9654
Tor Nærland <tor.nerland@nav.no>
2019-10-18 16:55:06 +0200

    Implementert OppgaveClient og oppdatert OppgaveService til å gå mot det nye rest-grensesnittet. Fjernet opprettOppgave fra OppgaveController
    Gjenstår feilhåndtering.

--- src/main/java/no/nav/familie/ks/oppslag/oppgave/OppgaveController.java
+++ src/main/java/no/nav/familie/ks/oppslag/oppgave/OppgaveController.java
@@ -1,8 +1,8 @@
 package no.nav.familie.ks.oppslag.oppgave;
 
 import no.nav.familie.ks.kontrakter.oppgave.Oppgave;
+import no.nav.familie.ks.kontrakter.oppgave.OppgaveKt;
 import no.nav.security.oidc.api.ProtectedWithClaims;
-import no.nav.security.oidc.api.Unprotected;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,16 +18,8 @@ public class OppgaveController {
         this.oppgaveService = oppgaveService;
     }
 
-    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, path = "/opprett")
-    public ResponseEntity<String> opprettOppgave(@RequestBody Oppgave request) {
-        return oppgaveService.opprettEllerOppdaterOppgave(request);
-    }
-
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, path = "/oppdater")
     public ResponseEntity oppdaterOppgave(@RequestBody Oppgave request) {
-        if (request.getEksisterendeOppgaveId() == null) {
-            return ResponseEntity.badRequest().header("message", "Mangler oppgaveId").build();
-        }
         return oppgaveService.opprettEllerOppdaterOppgave(request);
     }
 }
