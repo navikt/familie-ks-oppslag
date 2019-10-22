cac889a72d330665a2c51efb9b0673a6ab564a45
Helga Nyrud <helga.nyrud@nav.no>
2019-10-22 12:27:29 +0200

    Endrer hvordan vi henter ut kjønn

--- src/main/java/no/nav/familie/ks/oppslag/personopplysning/PersonopplysningerService.java
+++ src/main/java/no/nav/familie/ks/oppslag/personopplysning/PersonopplysningerService.java
@@ -57,7 +57,6 @@ public class PersonopplysningerService {
             request.setAktoer(new PersonIdent().withIdent(new NorskIdent().withIdent(personIdent)));
             request.setPeriode(new Periode().withFom(DateUtil.convertToXMLGregorianCalendar(fom)).withTom(DateUtil.convertToXMLGregorianCalendar(tom)));
             var response = personConsumer.hentPersonhistorikkResponse(request);
-            response.getBostedsadressePeriodeListe().forEach(periode -> secureLogger.info("Bostedsadresse for " + personIdent + ": fom " + periode.getPeriode().getFom() + " tom " + periode.getPeriode().getTom()));
             return ResponseEntity.ok(oversetter.tilPersonhistorikkInfo(new no.nav.familie.ks.oppslag.personopplysning.domene.PersonIdent(personIdent), response));
         } catch (HentPersonhistorikkSikkerhetsbegrensning exception) {
             LOG.info("Ikke tilgang til å hente historikk for person");
@@ -74,6 +73,7 @@ public class PersonopplysningerService {
                     .withAktoer(new PersonIdent().withIdent(new NorskIdent().withIdent(personIdent)))
                     .withInformasjonsbehov(List.of(Informasjonsbehov.FAMILIERELASJONER, Informasjonsbehov.ADRESSE));
             HentPersonResponse response = personConsumer.hentPersonResponse(request);
+            secureLogger.info("Person " + personIdent + " sitt kjønn er: " + response.getPerson().getKjoenn().getKjoenn().getValue());
             return ResponseEntity.ok(oversetter.tilPersoninfo(new no.nav.familie.ks.oppslag.personopplysning.domene.PersonIdent(personIdent), response));
         } catch (HentPersonSikkerhetsbegrensning exception) {
             LOG.info("Ikke tilgang til å hente personinfo for person");
